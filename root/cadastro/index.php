c6d2cdd3baf0c6df6bc19bf5c5f1a426df293eba
ATobolsky <51061937+ATobolsky@users.noreply.github.com>
2019-10-04 10:22:14 -0300

    Site atualizando formularios
    falta completar o form

--- root/cadastro/index.php
+++ root/cadastro/index.php
@@ -25,9 +25,50 @@
         
         <!--Formulario de cadastro-->
         <div class="row container">
+            <p>&nbsp;</p>
             <form action="banco_de_dados/create.php" method="post" class="col s12"></form>
             <fieldset class="formulario">
-                <legend>Cadastro</legend>
+                <legend><img src="imagem/cadastro-cliente.png" alt="(imagem)" width="100"></legend>
+                <h5 class="light center">Cadastro de Clientes</h5>
+<!--                Campo nome-->
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">account_circle</i>
+                    <input class="small" type="text" name="nome" id="nome" required autofocus>
+                    <label class="small" for="nome">Nome do Cliente</label>
+                </div>
+<!--                    campo email-->
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">email_circle</i>
+                    <input type="email" name="email" id="email" maxlength="50" required>
+                    <label for="email">Email do Cliente</label>
+                </div>
+                    <!--campo email-->
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">phone_circle</i>
+                    <input type="tel" name="telefone" id="telefone" maxlength="50" required>
+                    <label for="email">Telefone do Cliente</label>
+                </div>
+                        <!--campo nascimento-->
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">date_range</i>
+                    <input type="date" name="nascimento" maxlength="20" required>
+                    <label for="nascimento">Data de Nasacimento</label>
+                </div>
+                    <!--campo endereço-->
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">home</i>
+                    <input type="text" name="endereco" required>
+                    <label for="endereco">Endereço</label>
+                    <input type="number" name="number">
+                </div>
+                <div>
+                   
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" class="btn blue">
+                    <input type="reset" value="Limpar" class="btn red">     
+                </div>    
+                        
             </fieldset>
             
         </div>
